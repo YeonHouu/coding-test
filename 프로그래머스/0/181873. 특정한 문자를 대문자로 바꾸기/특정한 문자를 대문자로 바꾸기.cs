1f45feb6e6b0dd104c9f6932ca5ec1e61fefca93
YeonHouu <yeonhhou@gmail.com>
2025-05-22 14:53:05 +0900

    [level 0] Title: 특정한 문자를 대문자로 바꾸기, Time: 1.12 ms, Memory: 30.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181873. 특정한 문자를 대문자로 바꾸기/특정한 문자를 대문자로 바꾸기.cs
@@ -0,0 +1,8 @@
+using System;
+
+public class Solution {
+    public string solution(string my_string, string alp) {
+               
+        return my_string = my_string.Replace(alp, alp.ToUpper());
+    }
+}
