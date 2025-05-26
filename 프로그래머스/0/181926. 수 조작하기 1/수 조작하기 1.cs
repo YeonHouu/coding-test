1c145632c435484198da978a59cd71c12d425414
YeonHouu <yeonhhou@gmail.com>
2025-05-26 17:21:58 +0900

    [level 0] Title: 수 조작하기 1, Time: 1.30 ms, Memory: 31.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.cs
@@ -0,0 +1,26 @@
+using System;
+
+public class Solution {
+    public int solution(int n, string control) {
+        
+        foreach (char c in control)
+        {
+            switch (c)
+            {
+                case 'w':
+                    n += 1;
+                    break;
+                case 's':
+                    n -= 1;
+                    break;
+                case 'd':
+                    n += 10;
+                    break;
+                case 'a':
+                    n-=10;
+                    break;
+            }
+        }
+        return n;
+    }
+}
